6c2dc2d10b8d6adcfc6912479fa65745faafa801
Vanessa Weinelt <szitastamas@hotmail.com>
2019-07-27 20:40:02 +0200

    new file:   fontawesome-free-5.9.0-web/LICENSE.txt 	new file:   fontawesome-free-5.9.0-web/css/all.css 	new file:   fontawesome-free-5.9.0-web/css/all.min.css 	new file:   fontawesome-free-5.9.0-web/css/brands.css 	new file:   fontawesome-free-5.9.0-web/css/brands.min.css 	new file:   fontawesome-free-5.9.0-web/css/fontawesome.css 	new file:   fontawesome-free-5.9.0-web/css/fontawesome.min.css 	new file:   fontawesome-free-5.9.0-web/css/regular.css 	new file:   fontawesome-free-5.9.0-web/css/regular.min.css 	new file:   fontawesome-free-5.9.0-web/css/solid.css 	new file:   fontawesome-free-5.9.0-web/css/solid.min.css 	new file:   fontawesome-free-5.9.0-web/css/svg-with-js.css 	new file:   fontawesome-free-5.9.0-web/css/svg-with-js.min.css 	new file:   fontawesome-free-5.9.0-web/css/v4-shims.css 	new file:   fontawesome-free-5.9.0-web/css/v4-shims.min.css 	new file:   fontawesome-free-5.9.0-web/metadata/categories.yml 	new file:   fontawesome-free-5.9.0-web/metadata/icons.json 	new file:   fontawesome-free-5.9.0-web/metadata/icons.yml 	new file:   fontawesome-free-5.9.0-web/metadata/shims.json 	new file:   fontawesome-free-5.9.0-web/metadata/shims.yml 	new file:   fontawesome-free-5.9.0-web/metadata/sponsors.yml 	new file:   fontawesome-free-5.9.0-web/webfonts/fa-brands-400.eot 	new file:   fontawesome-free-5.9.0-web/webfonts/fa-brands-400.svg 	new file:   fontawesome-free-5.9.0-web/webfonts/fa-brands-400.ttf 	new file:   fontawesome-free-5.9.0-web/webfonts/fa-brands-400.woff 	new file:   fontawesome-free-5.9.0-web/webfonts/fa-brands-400.woff2 	new file:   fontawesome-free-5.9.0-web/webfonts/fa-regular-400.eot 	new file:   fontawesome-free-5.9.0-web/webfonts/fa-regular-400.svg 	new file:   fontawesome-free-5.9.0-web/webfonts/fa-regular-400.ttf 	new file:   fontawesome-free-5.9.0-web/webfonts/fa-regular-400.woff 	new file:   fontawesome-free-5.9.0-web/webfonts/fa-regular-400.woff2 	new file:   fontawesome-free-5.9.0-web/webfonts/fa-solid-900.eot 	new file:   fontawesome-free-5.9.0-web/webfonts/fa-solid-900.svg 	new file:   fontawesome-free-5.9.0-web/webfonts/fa-solid-900.ttf 	new file:   fontawesome-free-5.9.0-web/webfonts/fa-solid-900.woff 	new file:   fontawesome-free-5.9.0-web/webfonts/fa-solid-900.woff2 	new file:   img/bg1.jpg 	new file:   img/bg2.jpg 	new file:   img/buffet-celebration-delicious-5929.jpg 	new file:   img/camping.jpg 	new file:   img/feast.jpg 	new file:   img/firework1.jpg 	new file:   img/firework2.jpg 	new file:   img/james-bold-174ZYtxCtr4-unsplash.jpg 	new file:   img/photos-by-lanty-O38Id_cyV4M-unsplash.jpg 	new file:   img/randomlogo.jpg 	new file:   index.html 	new file:   main.js 	new file:   style.css

---
+++ main.js
@@ -0,0 +1,50 @@
+const aboutContainer = document.querySelector('.about-us-container');
+const landingContainer = document.querySelector('.landing-text-container');
+const landingTitle = document.querySelectorAll('.landing-text');
+const landingSubTitle = document.querySelector('.landing-subtitle');
+const landingIcon = document.querySelector('.arrow-down');
+const eventsContainer = document.querySelector('.events-container')
+const eventCards = document.querySelector('.event-cards');
+
+function landing(element){
+    element.style.opacity="1";
+    element.style.display="flex";
+}
+
+let lastTitle = landingTitle.length-1;
+
+window.onload = function(){
+    landingTitle.forEach(item => item.style.opacity="1");
+    landing(landingIcon);
+    landing(landingSubTitle);
+}
+
+const isInViewport = el => {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= 
+            (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth ||
+            document.documentElement.clientWidth)    
+    );
+};
+
+window.onscroll = function() {
+    if(isInViewport(aboutContainer)){
+        aboutContainer.style.transform="none";
+        aboutContainer.style.opacity="1";
+    }
+
+    if(isInViewport(eventsContainer)){
+        eventCards.style.transform="none";
+    }
+
+    landingContainer.style.transform='translateY(' + (window.pageYOffset * 0.6) + 'px)';
+}
+
+function shutUp(){
+    document.querySelectorAll('.is-active').forEach(element => element.classList.remove('is-active'));
+    document.querySelectorAll('.sliding').forEach(item => item.classList.remove('sliding'));
+}
