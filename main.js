40e8788f4e1d534c93ff82a1c6dae730518f21c5
Szitás Tamás <39591088+szitastamas@users.noreply.github.com>
2019-08-06 13:56:29 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,47 +3,47 @@ const landingContainer = document.querySelector('.landing-text-container');
 const landingTitle = document.querySelectorAll('.landing-text');
 const landingSubTitle = document.querySelector('.landing-subtitle');
 const landingIcon = document.querySelector('.arrow-down');
-const eventsContainer = document.querySelector('.events-container')
+const eventsContainer = document.querySelector('.events-container');
 const eventCards = document.querySelector('.event-cards');
 
-function landing(element){
-    element.style.opacity="1";
-    element.style.display="flex";
+function landing(element) {
+  element.style.opacity = '1';
+  element.style.display = 'flex';
 }
 
-window.onload = setTimout(function(){
-    landingTitle.forEach(item => item.style.opacity="1");
-    landing(landingIcon);
-    landing(landingSubTitle);
-    ,1000)
-}
+window.onload = () => {
+  landingTitle.forEach(item => (item.style.opacity = '1'));
+  landing(landingIcon);
+  landing(landingSubTitle);
+};
 
 const isInViewport = el => {
-    const rect = el.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= 
-            (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth ||
-            document.documentElement.clientWidth)    
-    );
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
 };
 
 window.onscroll = function() {
-    if(isInViewport(aboutContainer)){
-        aboutContainer.style.transform="none";
-        aboutContainer.style.opacity="1";
-    }
+  if (isInViewport(aboutContainer)) {
+    aboutContainer.style.transform = 'none';
+    aboutContainer.style.opacity = '1';
+  }
 
-    if(isInViewport(eventsContainer)){
-        eventCards.style.transform="none";
-        eventCards.style.opacity="1";
-    }
+  if (isInViewport(eventsContainer)) {
+    eventCards.style.transform = 'none';
+    eventCards.style.opacity = '1';
+  }
 
-    if(window.innerWidth > 868){
-        landingContainer.style.transform='translateY(' + (window.pageYOffset * 0.6) + 'px)';
-    }else{
-        landingContainer.style.transform='translateY(' + (window.pageYOffset * 0.2) + 'px)';
-    }
-}
+  if (window.innerWidth > 868) {
+    landingContainer.style.transform =
+      'translateY(' + window.pageYOffset * 0.6 + 'px)';
+  } else {
+    landingContainer.style.transform =
+      'translateY(' + window.pageYOffset * 0.2 + 'px)';
+  }
+};
